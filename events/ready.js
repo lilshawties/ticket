3612c223830960ede08273ac1cd300d0cac275a8
bomie <bomidelrey@gmail.com>
2024-05-23 10:28:49 +0700

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -45,8 +45,8 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`Tickets`, 
-  type: ActivityType.Listening }), 10000);
+  name:`.𝘣𝘰𝘮𝘪𝜗𝜚˚⋆`, 
+  type: ActivityType.Playing }), 10000);
 client.errorLog = config.errorLog
   
 }
